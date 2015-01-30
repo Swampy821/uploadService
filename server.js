3e0239a0c1f712f1dc2e26bec9044df07253cfad
Steve Marsh <Zimdale@gmail.com>
2015-01-30 08:30:47 -0500

    Removed seperate read server and began refactoring

--- server.js
+++ server.js
@@ -1,45 +1,13 @@
-var Hapi = require('hapi'),
-    fs = require('fs'),
-    crypto = require('crypto'),
-    cp = require('child_process');;
+var Hapi = require('hapi');
 
 var server = new Hapi.Server(),
     port = 3001;
 
 server.connection({port:port});
 
-
-server.route({
-    method: 'POST',
-    path: '/upload',
-    config: {
-        payload: {
-            output: 'stream',
-            parse: true
-        }
-    },
-    handler: uploadHandler
-});
-
-
-function uploadHandler(request, reply) {
-    var hash = crypto.createHash('md5'),
-        tStamp = Math.floor(new Date() / 1000),
-        remoteAddr = request.info.remoteAddress,
-        finalHash = hash.update(tStamp.toString()).update(remoteAddr).digest('hex'),
-        file = request.payload['file'],
-        fileName = file.hapi.filename,
-        extension = fileName.substr(fileName.lastIndexOf('.') + 1),
-        saveFile = finalHash + '.' + extension;
-    request.payload['file'].pipe(fs.createWriteStream('./files/' + saveFile));
-
-    reply({
-        "file":saveFile
-    });
-}
+server = require('./lib/routes.js')(server);
 
 
 server.start(function() {
     console.log('Upload service started on port ' + port);
-    cp.fork(__dirname + '/readService.js');
 });
